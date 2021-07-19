e7c1894e847b3bcecddb6c92d386888289ac8f99
Glen <mistergjones@yahoo.com.au>
2021-07-19 22:32:17 +1000

    Added Song

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
 import SaveTheDate from "./Components/SaveTheDate";
 import RSVPModal from "./Components/RSVPModal";
+import gj from "./Components/LetsStayTogether.mp3";
 
 function App() {
     return (
         <>
+            <center>
+                <div>
+                    <audio src={gj} type="audio/mp3" controls autoPlay />
+                </div>
+            </center>
             <SaveTheDate />
 
             {/* <RSVPModal /> */}
